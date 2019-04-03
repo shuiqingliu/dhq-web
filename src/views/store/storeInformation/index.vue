8b0fee38302c6590feaeb7a1d1e1c883ff3b9bce
llllllzx <llllllllll_zx@163.com>
2019-04-03 11:05:15 +0800

    规划页面

--- src/views/store/storeInformation/index.vue
+++ src/views/store/storeInformation/index.vue
@@ -1,103 +1,219 @@
-<template>
+<template> 
   <div class="app-container">
-   {{res}}
+    <el-card class="filter-container" shadow="never">
+        <div>
+          <i class="el-icon-search"></i>
+          <span>筛选搜索</span>
+          <el-button
+            style="float: right"
+            @click="searchBrandList()"
+            type="primary"
+            size="small">
+            查询结果
+          </el-button>
+        </div>
+        <div style="margin-top: 15px">
+          <el-form :inline="true" :model="listQuery" size="small" label-width="140px">
+            <el-form-item label="输入搜索：">
+              <el-input style="width: 203px" v-model="listQuery.keyword" placeholder="品牌名称/关键字"></el-input>
+            </el-form-item>
+          </el-form>
+        </div>
+    </el-card>
+    <el-card class="operate-container" shadow="never">
+      <i class="el-icon-tickets"></i>
+      <span>数据列表</span>
+      <el-button
+        class="btn-add"
+        @click="addBrand()"
+        size="mini">
+        添加
+      </el-button>
+    </el-card>
+    <div class="table-container">
+      <el-table ref="brandTable"
+                :data="list"
+                style="width: 100%"
+                @selection-change="handleSelectionChange"
+                v-loading="listLoading"
+                border>
+        <el-table-column type="selection" width="60" align="center"></el-table-column>
+        <el-table-column label="编号" width="100" align="center">
+          <template slot-scope="scope">{{scope.row.id}}</template>
+        </el-table-column>
+        <el-table-column label="品牌名称" align="center">
+          <template slot-scope="scope">{{scope.row.name}}</template>
+        </el-table-column>
+        <el-table-column label="品牌首字母" width="200" align="center">
+          <template slot-scope="scope">{{scope.row.firstLetter}}</template>
+        </el-table-column>
+        <el-table-column label="排序" width="200" align="center">
+          <template slot-scope="scope">{{scope.row.sort}}</template>
+        </el-table-column>
+        <el-table-column label="操作" width="200" align="center">
+          <template slot-scope="scope">
+            <el-button
+              size="mini"
+              @click="handleUpdate(scope.$index, scope.row)">编辑
+            </el-button>
+            <el-button
+              size="mini"
+              type="danger"
+              @click="handleDelete(scope.$index, scope.row)">删除
+            </el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </div>
+    <div class="batch-operate-container">
+      <el-select
+        size="small"
+        v-model="operateType" placeholder="批量操作666">
+        <el-option
+          v-for="item in operates"
+          :key="item.value"
+          :label="item.label"
+          :value="item.value">
+        </el-option>
+      </el-select>
+      <el-button
+        style="margin-left: 20px"
+        class="search-button"
+        @click="handleBatchOperate()"
+        type="primary"
+        size="small">
+        确定
+      </el-button>
+    </div>
+    <div class="pagination-container">
+      <el-pagination
+        background
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        layout="total, sizes,prev, pager, next,jumper"
+        :page-size="listQuery.pageSize"
+        :page-sizes="[5,10,15]"
+        :current-page.sync="listQuery.pageNum"
+        :total="total">
+      </el-pagination>
+    </div>
   </div>
 </template>
-
 <script>
+  import {fetchList, updateShowStatus, updateFactoryStatus, deleteBrand} from '@/api/brand'
+
   export default {
-    name: 'addStore',
-    data(){
+    name: 'brandList',
+    data() {
       return {
-        res: '这里是门店信息'
+        operates: [
+          {
+            label: "批量删除",
+            value: "0"
+          }
+        ],
+        operateType: null,
+        listQuery: {
+          keyword: null,
+          pageNum: 1,
+          pageSize: 5
+        },
+        list: null,
+        total: null,
+        listLoading: true,
+        multipleSelection: []
+      }
+    },
+    created() {
+      this.getList();
+    },
+    methods: {
+      getList() {
+        this.listLoading = true;
+        fetchList(this.listQuery).then(response => {
+          this.listLoading = false;
+          this.list = response.data.list;
+          this.total = response.data.total;
+          this.totalPage = response.data.totalPage;
+          this.pageSize = response.data.pageSize;
+        });
+      },
+      handleSelectionChange(val) {
+        this.multipleSelection = val;
+      },
+      //更新
+      handleUpdate(index, row) {
+        this.$router.push({path: '/pms/updateBrand', query: {id: row.id}})
+      },
+      //删除
+      handleDelete(index, row) {
+        this.$confirm('是否要删除该品牌', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+          deleteBrand(row.id).then(response => {
+            this.$message({
+              message: '删除成功',
+              type: 'success',
+              duration: 1000
+            });
+            this.getList();
+          });
+        });
+      },
+      //处理改变分页
+      handleSizeChange(val) {
+        this.listQuery.pageNum = 1;
+        this.listQuery.pageSize = val;
+        this.getList();
+      },
+      handleCurrentChange(val) {
+        this.listQuery.pageNum = val;
+        this.getList();
+      },
+      //查询
+      searchBrandList() {
+        this.listQuery.pageNum = 1;
+        this.getList();
+      },
+      //处理批量操作
+      handleBatchOperate() {
+        console.log(this.multipleSelection);
+        if (this.multipleSelection < 1) {
+          this.$message({
+            message: '请选择一条记录',
+            type: 'warning',
+            duration: 1000
+          });
+          return;
+        }
+        //用来判断是选中了哪个批量操作!!!!!!!!!!!!!（这个没有实现批量删除）
+        let ids = [];
+        for (let i = 0; i < this.multipleSelection.length; i++) {
+          ids.push(this.multipleSelection[i].id);
+        }
+        if(this.operateType===0){
+          //删除
+         // this.deleteHomeAdvertise(ids);
+         //在这里重新写一个函数
+        }else {
+          this.$message({
+            message: '请选择批量操作类型',
+            type: 'warning',
+            duration: 1000
+          });
+        }
+      },
+      addBrand() {
+        this.$router.push({path: '/pms/addBrand'})
       }
     }
   }
 </script>
+<style rel="stylesheet/scss" lang="scss" scoped>
 
-<style scoped>
-  .app-container {
-    margin-top: 40px;
-    margin-left: 120px;
-    margin-right: 120px;
-  }
 
-  .address-layout {
-  }
-
-  .total-layout {
-    margin-top: 20px;
-  }
-
-  .total-frame {
-    border: 1px solid #DCDFE6;
-    padding: 20px;
-    height: 100px;
-  }
-
-  .total-icon {
-    color: #409EFF;
-    width: 60px;
-    height: 60px;
-  }
-
-  .total-title {
-    position: relative;
-    font-size: 16px;
-    color: #909399;
-    left: 70px;
-    top: -50px;
-  }
-
-  .total-value {
-    position: relative;
-    font-size: 18px;
-    color: #606266;
-    left: 70px;
-    top: -40px;
-  }
-
-  .un-handle-layout {
-    margin-top: 20px;
-    border: 1px solid #DCDFE6;
-  }
-
-  .layout-title {
-    color: #606266;
-    padding: 15px 20px;
-    background: #F2F6FC;
-    font-weight: bold;
-  }
-
-  .un-handle-content {
-    padding: 20px 40px;
-  }
-
-  .un-handle-item {
-    border-bottom: 1px solid #EBEEF5;
-    padding: 10px;
-  }
-
-  .overview-layout {
-    margin-top: 20px;
-  }
-
-  .overview-item-value {
-    font-size: 24px;
-    text-align: center;
-  }
-
-  .overview-item-title {
-    margin-top: 10px;
-    text-align: center;
-  }
-
-  .out-border {
-    border: 1px solid #DCDFE6;
-  }
-
-  .statistics-layout {
-    margin-top: 20px;
-    border: 1px solid #DCDFE6;
-  }
 </style>
+
+
