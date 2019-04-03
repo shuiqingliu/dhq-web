8d46e3408d9ee7b2f05958da1cb2042ea235b7f7
llllllzx <llllllllll_zx@163.com>
2019-04-03 18:56:27 +0800

    修改了函数名字

--- src/api/storeInformation.js
+++ src/api/storeInformation.js
@@ -1,11 +1,13 @@
 import request from '@/utils/request'
+//获取门店信息列表
 export function fetchList(params) {
   return request({
-    url:'/brand/list',
+    url:'/storeinfo/list',
     method:'get',
     params:params
   })
 }
+//创建门店信息
 export function createStoreInfo(data) {
   return request({
     url:'/storeinfo/create',
@@ -13,6 +15,7 @@ export function createStoreInfo(data) {
     data:data
   })
 }
+//修改门店信息
 export function updateStoreInfo(data) {
   return request({
     url:'/storeinfo/update',
@@ -20,26 +23,17 @@ export function updateStoreInfo(data) {
     data:data
   })
 }
-
+//删除门店信息
 export function deleteStoreInfo(id) {
   return request({
     url:'/storeinfo/deleteByID/'+id,
     method:'get',
   })
 }
-
-export function getBrand(id) {
+//根据id获取获取门店信息
+export function getStoreInfo(id) {
   return request({
     url:'/brand/'+id,
     method:'get',
   })
 }
-
-export function updateBrand(id,data) {
-  return request({
-    url:'/brand/update/'+id,
-    method:'post',
-    data:data
-  })
-}
-
