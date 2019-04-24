bfdd1a6394d03e008130824bba288a47c68bee36
llllllzx <llllllllll_zx@163.com>
2019-04-24 18:26:45 +0800

    api

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  BASE_API: '"http://10.103.241.66:2140"'
-  //BASE_API: '" http://localhost:8080"'
+  //BASE_API: '"http://10.103.241.66:2140"'
+  BASE_API: '" http://localhost:8080"'
 }
