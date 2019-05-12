b9e66b38748a3d53d07cfea5a83d90355e5d8c12
xialeiww <735087130@qq.com>
2019-05-12 22:41:46 +0800

    完善

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,14 +4,8 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-<<<<<<< HEAD
  BASE_API: '"http://10.103.241.66:2140"'
 //  BASE_API: '"http://10.128.222.211:2140"'
   //BASE_API: '"http://localhost:8080"' 
   // BASE_API: '"http://10.210.2.104:8080"'
 })
-=======
-  // BASE_API: '"http://localhost:8080"'
-  BASE_API: '"http://10.103.241.66:2140"' 
-})
->>>>>>> 3a26ecbd615e15671af2de2796037cb43c26e932
