4591f2018d542a3b0f4e7efce950b80c460727c9
liuzhaoxin_i <liuzhaoxin_i@didiglobal.com>
2019-05-20 16:17:09 +0800

    0520second

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-'use strict'
-const merge = require('webpack-merge')
-const prodEnv = require('./prod.env')
-
-module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"',
-  // BASE_API: '"http://localhost:8080"'
-  BASE_API: '"http://10.103.241.66:2140"' 
-})
-=======
 'use strict'
 const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
@@ -18,4 +7,3 @@ module.exports = merge(prodEnv, {
   //BASE_API: '"http://localhost:2140"'
   BASE_API: '"http://10.103.241.66:2140"' 
 })
->>>>>>> feature_test
