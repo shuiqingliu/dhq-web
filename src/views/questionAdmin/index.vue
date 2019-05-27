12f67d9f180b1c481feed7c3af5439e7a6584440
xialeiww <735087130@qq.com>
2019-05-27 19:20:15 +0800

    修改bug3

--- src/views/questionAdmin/index.vue
+++ src/views/questionAdmin/index.vue
@@ -111,7 +111,7 @@
            <template slot-scope="scope">{{timu_type[state-1]}}</template>
           </el-table-column>
           <el-table-column label="所属科目" align="center">
-            <template slot-scope="scope">{{scnoope.row.subject}}</template>
+            <template slot-scope="scope">{{scope.row.subject}}</template>
           </el-table-column>
           <el-table-column label="所属科目" align="center">
             <template slot-scope="scope">{{scope.row.subject}}</template>
