5db7ef7500afb827fbb2a9585a9d44eea450712d
liuzhaoxin_i <liuzhaoxin_i@didiglobal.com>
2019-05-31 23:32:12 +0800

    修改设备实例和门店设备分配

--- src/api/equipmentApply.js
+++ src/api/equipmentApply.js
@@ -27,38 +27,6 @@ export function batchAllocateDevice(params) {
     params: params
   })
 }
-
-
-//创建设备
-export function createEquipmentInstance(data) {
-  return request({
-    url: '/device/add',
-    method: 'post',
-    data: data
-  })
-}
-//修改设备
-export function updateEquipmentInstance(id, data) {
-  return request({
-    url: '/device/update/' + id,
-    method: 'post',
-    data: data
-  })
-}
-//删除设备
-export function deleteEquipmentInstance(id) {
-  return request({
-    url: '/device/delete?id=' + id,
-    method: 'get',
-  })
-}
-//批量删除设备
-export function batchDeleteEquipmentInstance(ids) {
-  return request({
-    url: '/device/delete/batch?ids=' + ids,
-    method: 'post',
-  })
-}
 //根据设备类型ID查看未分配的设备。
 export function findDevicesWithType(params) {
   return request({
@@ -67,25 +35,57 @@ export function findDevicesWithType(params) {
     params: params
   })
 }
-//获取一级列表
-export function getFirstCategory() {
-  return request({
-    url: '/device/getFirstCategory',
-    method: 'get'
-  })
-}
-//获取二级列表
-export function getSecondCategory() {
-  return request({
-    url: '/device/getSecondCategory',
-    method: 'get'
-  })
-}
 
-//获取三级列表
-export function getThirdCategory() {
-  return request({
-    url: '/device/getThirdCategory',
-    method: 'get'
-  })
-}
+// //创建设备
+// export function createEquipmentInstance(data) {
+//   return request({
+//     url: '/device/add',
+//     method: 'post',
+//     data: data
+//   })
+// }
+// //修改设备
+// export function updateEquipmentInstance(id, data) {
+//   return request({
+//     url: '/device/update/' + id,
+//     method: 'post',
+//     data: data
+//   })
+// }
+// //删除设备
+// export function deleteEquipmentInstance(id) {
+//   return request({
+//     url: '/device/delete?id=' + id,
+//     method: 'get',
+//   })
+// }
+// //批量删除设备
+// export function batchDeleteEquipmentInstance(ids) {
+//   return request({
+//     url: '/device/delete/batch?ids=' + ids,
+//     method: 'post',
+//   })
+// }
+
+// //获取一级列表
+// export function getFirstCategory() {
+//   return request({
+//     url: '/device/getFirstCategory',
+//     method: 'get'
+//   })
+// }
+// //获取二级列表
+// export function getSecondCategory() {
+//   return request({
+//     url: '/device/getSecondCategory',
+//     method: 'get'
+//   })
+// }
+
+// //获取三级列表
+// export function getThirdCategory() {
+//   return request({
+//     url: '/device/getThirdCategory',
+//     method: 'get'
+//   })
+// }
