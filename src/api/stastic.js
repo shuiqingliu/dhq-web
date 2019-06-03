e5eb9652b070c065c2814273c7dcb8f9476cdca4
xialeiww <735087130@qq.com>
2019-06-03 18:34:40 +0800

    统计模块完善

--- src/api/stastic.js
+++ src/api/stastic.js
@@ -2,51 +2,109 @@ import request from '@/utils/request'
 
 //课程数据
 //1.课程课程
-export function KCXL(data) {
+export function KCXL(params) {
     return request({
       url:'/OfflineCourseStatistic/courseAndCourse',
-      method:'post',
-      data:data
+      method:'get',
+      params:params
     })
 }
 //2.课程地区
-export function KCDQ(data) {
+export function KCDQ(params) {
     return request({
       url:'/OfflineCourseStatistic/courseAndRegion',
-      method:'post',
-      data:data
+      method:'get',
+      params:params
     })
 }
 //课程门店
-export function KCMD(data) {
+export function KCMD(params) {
     return request({
       url:'/OfflineCourseStatistic/courseAndShop',
-      method:'post',
-      data:data
+      method:'get',
+      params:params
     })
+}
+//课程按月
+export function KCY(params) {
+  return request({
+    url:'/OfflineCourseStatistic/getByYearWithMonth',
+    method:'get',
+    params:params
+  })
 }
 
 //设备地区
-export function SBDQ(data) {
+export function SBDQ(params) {
     return request({
       url:'/deviceStatic/findDeviceNumberOfProvince',
-      method:'post',
-      data:data
+      method:'get',
+    params:params
     })
 }
 //设备前十
-export function SBQS(data) {
+export function SBQS(params) {
     return request({
       url:'/deviceStatic/findTopTenDeviceTypeList',
-      method:'post',
-      data:data
+      method:'get',
+    params:params
     })
 }
 //设备门店
-export function SBMD(data) {
+export function SBMD(params) {
     return request({
       url:'/deviceStatic/findDeviceNumberOfShop',
-      method:'post',
-      data:data
+      method:'get',
+    params:params
     })
+}
+//设备维修前十
+export function SBWXQS(params) {
+  return request({
+    url:'/deviceMaintainStatic/findTopTenMaintainDeviceTypeList',
+    method:'get',
+    params:params
+  })
+}
+//设备维修门店前十
+export function SBWXMD(params) {
+  return request({
+    url:'/deviceMaintainStatic/findTopTenMaintainShopList',
+    method:'get',
+    params:params
+  })
+}
+
+//财务地区
+export function CWDQ(params) {
+  return request({
+    url:'/financialStatic/findOfflineProfitByProvinceList',
+    method:'get',
+    params:params
+  })
+}
+//财务门店
+export function CWMD(params) {
+  return request({
+    url:'/financialStatic/findOfflineProfitTopTenShop',
+    method:'get',
+    params:params
+  })
+}
+
+//线上课前10
+export function XSKQS(params) {
+  return request({
+    url:'/OnlineCourseStatistic/getOnlineCourseCountByCourse',
+    method:'get',
+    params:params
+  })
+}
+//线上课月
+export function XSKY(params) {
+  return request({
+    url:'/OnlineCourseStatistic/getCountByYear',
+    method:'get',
+    params:params
+  })
 }
