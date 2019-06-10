a0b5ef891e486eeeb88d02ccd53aef1c47b71102
xialei <735087130@qq.com>
2019-06-10 23:44:24 +0800

    解决冲突

--- config/dev.env.js
+++ config/dev.env.js
@@ -5,7 +5,7 @@ const prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   //BASE_API: '"http://localhost:2140"'
-  BASE_API: '"http://10.103.241.66:2140"' 
+  BASE_API: '"http://10.103.250.120:2140"' 
   // BASE_API: '"http://192.168.43.146:2140"'
 
 })
