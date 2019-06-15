e3332edf34242ddf056de4567729a8cc23b88bfb
xialeiww <735087130@qq.com>
2019-06-15 21:49:56 +0800

    改了图片接口地址

--- src/components/Tinymce/components/editorImage.vue
+++ src/components/Tinymce/components/editorImage.vue
@@ -45,7 +45,7 @@ export default {
       dialogVisible: false,
       listObj: {},
       fileList: [],
-      actionUrl: 'http://10.28.196.217:2140/upload/pic'
+      actionUrl: 'http://10.103.250.120:2140/upload/pic'
     }
   },
   mounted() {
