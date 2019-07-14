ae02499551f1d83a0a8ee31da9ac864fe61d6041
liuzhaoxin_i <liuzhaoxin_i@didiglobal.com>
2019-07-14 17:06:12 +0800

    添加前端上传组件

--- config/dev.env.js
+++ config/dev.env.js
@@ -7,7 +7,7 @@ module.exports = merge(prodEnv, {
   //BASE_API: '"http://localhost:2140"'
   //孙佳林后台地址
   // BASE_API: '"http://10.210.2.104:2140"' 
-  BASE_API: '"http://60.205.167.19:8075"' 
-  //  BASE_API: '"http://10.28.235.18:2140"' 
+ // BASE_API: '"http://60.205.167.19:8075"' 
+  BASE_API: '"http://10.103.250.120:2140"' 
   // BASE_API: '"http://192.168.43.146:2140"'
 })
