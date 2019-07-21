765c27a84ab2a5e5681ba0f17ad039aea376cf9b
qingliu <shuiqingliu14@gmail.com>
2019-07-22 00:44:43 +0800

    echarts 按需加载

--- src/main.js
+++ src/main.js
@@ -5,7 +5,12 @@ import 'normalize.css/normalize.css'// A modern alternative to CSS resets
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import locale from 'element-ui/lib/locale/lang/zh-CN' // lang i18n
-import VCharts from 'v-charts'
+// import VCharts from 'v-charts'
+import VeLine from 'v-charts/lib/line'
+import VeMap from 'v-charts/lib/map.common'
+import VeBar from 'v-charts/lib/bar.common'
+import VePie from 'v-charts/lib/pie.common'
+
 
 import '@/styles/index.scss' // global css
 
@@ -17,7 +22,11 @@ import '@/icons' // icon
 import '@/permission' // permission control
 
 Vue.use(ElementUI, { locale })
-Vue.use(VCharts)
+Vue.component(VeBar.name,VeBar)
+Vue.component(VeLine.name,VeLine)
+Vue.component(VePie.name,VePie)
+Vue.component(VeMap.name,VeMap)
+
 
 Vue.config.productionTip = false
 
